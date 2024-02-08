949d8236ddb497494029cf52ede2c4bd372deef2
krika71 <krika7103@gmail.com>
2024-02-08 16:44:18 +0900

    投稿機能追加

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 //     return view('welcome');
 // });
 Route::get('/', [PostController::class, 'index']);
-Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
+
 ?>
 
