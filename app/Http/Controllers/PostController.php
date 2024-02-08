8d694b0fd90f53415e5828d03f77d6a413192721
krika71 <krika7103@gmail.com>
2024-02-08 15:44:35 +0900

    一覧表示機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,13 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    public function index(Post $post)
     {
-        return $post->get();//$postの中身を戻り値にする。
+        
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        //ぜんぶ　
+        //return view('posts.index')->with(['posts' => $post->get()]);  
+        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
 }
+?>
